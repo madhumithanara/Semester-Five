f48025f20a197ad0ea65f5fc95d05ec667033745
Madhumitha Nara <madhumithanara.171it125@nitk.edu.in>
2019-08-27 15:52:42 +0530

    DBS Lab 3

--- DBMS/Lab1/commands/ddl.sql
+++ DBMS/Lab1/commands/ddl.sql
@@ -1,131 +1,122 @@
+CREATE DATABASE COMPANY;
+USE COMPANY;
+CREATE TABLE EMPLOYEE
+( Fname           VARCHAR(10)   NOT NULL,
+  Minit           CHAR,
+  Lname           VARCHAR(20)      NOT NULL,
+  Ssn             CHAR(9)          NOT NULL,
+  Bdate           DATE,
+  Address         VARCHAR(30),
+  Sex             CHAR(1),
+  Salary          DECIMAL(5),
+  Super_ssn       CHAR(9),
+  Dno             INT               NOT NULL,
+PRIMARY KEY   (Ssn));
 
-/*
-** ----------------------------------------------------------------------------
-** script to create the employee table 
-** --------------------------------------------------------------------------*/
+CREATE TABLE DEPARTMENT
+( Dname           VARCHAR(15)       NOT NULL,
+  Dnumber         INT               NOT NULL,
+  Mgr_ssn         CHAR(9)           NOT NULL,
+  Mgr_start_date  DATE,
+PRIMARY KEY (Dnumber),
+UNIQUE      (Dname),
+FOREIGN KEY (Mgr_ssn) REFERENCES EMPLOYEE(Ssn) );
 
-CREATE TABLE employee (
-	fName			VARCHAR2(20)NOT NULL,
-	mInit			CHAR(1),
-  	lName     		VARCHAR2(20)  NOT NULL,
-	ssn			VARCHAR2(9)PRIMARY KEY,
-	bDate			DATE NOT NULL,
-	address 		VARCHAR(50) NOT NULL,
-	sex			CHAR(1)	NOT NULL CHECK (sex = 'F' OR sex = 'M'),
-	salary			FLOAT NOT NULL CHECK (salary > 0),
-	superSSN		VARCHAR2(9),
-	dNo			INTEGER,
-	hireDate 		DATE NOT NULL,
-	terminateDate 		DATE
-);
+CREATE TABLE DEPT_LOCATIONS
+( Dnumber         INT               NOT NULL,
+  Dlocation       VARCHAR(15)       NOT NULL,
+PRIMARY KEY (Dnumber, Dlocation),
+FOREIGN KEY (Dnumber) REFERENCES DEPARTMENT(Dnumber) );
 
-SHOW ERRORS;
+CREATE TABLE PROJECT
+( Pname           VARCHAR(15)       NOT NULL,
+  Pnumber         INT               NOT NULL,
+  Plocation       VARCHAR(15),
+  Dnum            INT               NOT NULL,
+PRIMARY KEY (Pnumber),
+UNIQUE      (Pname),
+FOREIGN KEY (Dnum) REFERENCES DEPARTMENT(Dnumber) );
 
-/*
-** ----------------------------------------------------------------------------
-** add foreign key for the employee table 
-** --------------------------------------------------------------------------*/
-ALTER TABLE employee
-	ADD (FOREIGN KEY (superSSN) REFERENCES employee(ssn) ON DELETE SET NULL);
+CREATE TABLE WORKS_ON
+( Essn            CHAR(9)           NOT NULL,
+  Pno             INT               NOT NULL,
+  Hours           DECIMAL(3,1)      NOT NULL,
+PRIMARY KEY (Essn, Pno),
+FOREIGN KEY (Essn) REFERENCES EMPLOYEE(Ssn),
+FOREIGN KEY (Pno) REFERENCES PROJECT(Pnumber) );
 
-SHOW ERRORS;
+CREATE TABLE DEPENDENT
+( Essn            CHAR(9)           NOT NULL,
+  Dependent_name  VARCHAR(15)       NOT NULL,
+  Sex             CHAR,
+  Bdate           DATE,
+  Relationship    VARCHAR(8),
+PRIMARY KEY (Essn, Dependent_name),
+FOREIGN KEY (Essn) REFERENCES EMPLOYEE(Ssn) );
 
-/*
-** ----------------------------------------------------------------------------
-** script to create the department table 
-** --------------------------------------------------------------------------*/
+INSERT INTO EMPLOYEE
+VALUES      ('John','B','Smith',123456789,'1965-01-09','731 Fondren, Houston TX','M',30000,333445555,5),
+            ('Franklin','T','Wong',333445555,'1965-12-08','638 Voss, Houston TX','M',40000,888665555,5),
+            ('Alicia','J','Zelaya',999887777,'1968-01-19','3321 Castle, Spring TX','F',25000,987654321,4),
+            ('Jennifer','S','Wallace',987654321,'1941-06-20','291 Berry, Bellaire TX','F',43000,888665555,4),
+            ('Ramesh','K','Narayan',666884444,'1962-09-15','975 Fire Oak, Humble TX','M',38000,333445555,5),
+            ('Joyce','A','English',453453453,'1972-07-31','5631 Rice, Houston TX','F',25000,333445555,5),
+            ('Ahmad','V','Jabbar',987987987,'1969-03-29','980 Dallas, Houston TX','M',25000,987654321,4),
+            ('James','E','Borg',888665555,'1937-11-10','450 Stone, Houston TX','M',55000,null,1);
 
-CREATE TABLE department (
-	dName			VARCHAR2(20) NOT NULL,
-	dNumber			INTEGER		PRIMARY KEY,
-	mgrSSN			VARCHAR2(9),
-	mgrStartDate		Date NOT NULL,
-	FOREIGN KEY (mgrSSN) REFERENCES employee(ssn) ON DELETE SET NULL
-);
+INSERT INTO DEPARTMENT
+VALUES      ('Research',5,333445555,'1988-05-22'),
+            ('Administration',4,987654321,'1995-01-01'),
+            ('Headquarters',1,888665555,'1981-06-19');
 
-SHOW ERRORS;
+INSERT INTO PROJECT
+VALUES      ('ProductX',1,'Bellaire',5),
+            ('ProductY',2,'Sugarland',5),
+            ('ProductZ',3,'Houston',5),
+            ('Computerization',10,'Stafford',4),
+            ('Reorganization',20,'Houston',1),
+            ('Newbenefits',30,'Stafford',4);
 
-/*
-** ----------------------------------------------------------------------------
-** script to create the dept_location table 
-** --------------------------------------------------------------------------*/
-CREATE TABLE deptLocation (
-	dNumber		INTEGER,
-	dLocation	VARCHAR2(20),
-	PRIMARY KEY (dNumber,dLocation),
-	FOREIGN KEY(dNumber) REFERENCES department(dNumber)
-);
-SHOW ERRORS;
+INSERT INTO WORKS_ON
+VALUES     (123456789,1,32.5),
+           (123456789,2,7.5),
+           (666884444,3,40.0),
+           (453453453,1,20.0),
+           (453453453,2,20.0),
+           (333445555,2,10.0),
+           (333445555,3,10.0),
+           (333445555,10,10.0),
+           (333445555,20,10.0),
+           (999887777,30,30.0),
+           (999887777,10,10.0),
+           (987987987,10,35.0),
+           (987987987,30,5.0),
+           (987654321,30,20.0),
+           (987654321,20,15.0),
+           (888665555,20,16.0);
 
-/*
-** ----------------------------------------------------------------------------
-** add foreign key for the employee table 
-** --------------------------------------------------------------------------*/
-ALTER TABLE employee
-	ADD (FOREIGN KEY (dNo) REFERENCES department(dNumber) ON DELETE SET NULL);
-	
-SHOW ERRORS;
+INSERT INTO DEPENDENT
+VALUES      (333445555,'Alice','F','1986-04-04','Daughter'),
+            (333445555,'Theodore','M','1983-10-25','Son'),
+            (333445555,'Joy','F','1958-05-03','Spouse'),
+            (987654321,'Abner','M','1942-02-28','Spouse'),
+            (123456789,'Michael','M','1988-01-04','Son'),
+            (123456789,'Alice','F','1988-12-30','Daughter'),
+            (123456789,'Elizabeth','F','1967-05-05','Spouse');
 
-/*
-** ----------------------------------------------------------------------------
-** script to create the project table 
-** --------------------------------------------------------------------------*/
+INSERT INTO DEPT_LOCATIONS
+VALUES      (1,'Houston'),
+            (4,'Stafford'),
+            (5,'Bellaire'),
+            (5,'Sugarland'),
+            (5,'Houston');
 
-CREATE TABLE project (
-	pName			VARCHAR(20)	NOT NULL,
-	pNumber			INTEGER  	PRIMARY KEY,
-	pLocation		VARCHAR(50)	NOT NULL,
-	dNum			INTEGER	,
-	budgetedHours  		FLOAT NOT NULL CHECK(budgetedHours > 0),
-	FOREIGN KEY(dNum) REFERENCES department(dNumber) ON DELETE SET NULL
-);
+ALTER TABLE DEPARTMENT
+ ADD CONSTRAINT Dep_emp FOREIGN KEY (Mgr_ssn) REFERENCES EMPLOYEE(Ssn);
 
-SHOW ERRORS;
-
-/*
-** ----------------------------------------------------------------------------
-** script to create the worksOn table 
-** --------------------------------------------------------------------------*/
-
-CREATE TABLE worksOn (
-	eSSN			VARCHAR2(9),
-	pNo			INTEGER	,
-	hoursSpent  		FLOAT NOT NULL CHECK(hoursSpent >= 0),
-	PRIMARY KEY (eSSN,pNo),
-	FOREIGN KEY(eSSN) REFERENCES employee(ssn) ON DELETE CASCADE,
-	FOREIGN KEY(pNo) REFERENCES project(pNumber) ON DELETE CASCADE
-);
-SHOW ERRORS;
-
-/*
-** ----------------------------------------------------------------------------
-** script to create the dependent table 
-** --------------------------------------------------------------------------*/
-
-CREATE TABLE dependent (
-	eSSN			VARCHAR2(9),
-	dependentName 		VARCHAR2(20),
-	sex			CHAR(1)	NOT NULL CHECK (sex = 'F' OR sex = 'M'),
-	bDate			DATE	NOT NULL,
-	relationship		VARCHAR2(20) NOT NULL,
-	PRIMARY KEY(eSSN,dependentName),
-	FOREIGN KEY(eSSN) REFERENCES employee(ssn) ON DELETE CASCADE
-);
-
-SHOW ERRORS;
-
-/*
-** ----------------------------------------------------------------------------
-** script to create the consultant table 
-** --------------------------------------------------------------------------*/
-
-CREATE TABLE consultant (
- eSSN       		VARCHAR2(9),
- consultantCompan	VARCHAR2(20),
- contractCost 		FLOAT NOT NULL CHECK(contractCost >=0),
- PRIMARY KEY(eSSN, consultantCompany),
- FOREIGN KEY(eSSN) REFERENCES employee(ssn) ON DELETE CASCADE
- );
- 
- SHOW ERRORS;
- 
+ALTER TABLE EMPLOYEE
+ ADD CONSTRAINT Emp_emp FOREIGN KEY (Super_ssn) REFERENCES EMPLOYEE(Ssn);
+ALTER TABLE EMPLOYEE
+ ADD CONSTRAINT Emp_dno FOREIGN KEY  (Dno) REFERENCES DEPARTMENT(Dnumber);
+ALTER TABLE EMPLOYEE
+ ADD CONSTRAINT Emp_super FOREIGN KEY  (Super_ssn) REFERENCES EMPLOYEE(Ssn);
---
+++ DBMS/Lab3/queries.sql
@@ -0,0 +1,44 @@
+USE COMPANY;
+
+-- 1) Retrieve the name, birthdate and address of every employee who works for the ‘administration’ department.
+SELECT Fname, Address, Bdate from EMPLOYEE, DEPARTMENT WHERE EMPLOYEE.Dno = DEPARTMENT.Dnumber and DEPARTMENT.Dname = 'Administration';
+
+-- 2) Find the sum of the salaries of all employees of the ‘Research’
+-- department, as well as the maximum salary, the minimum salary, and the average salary in this department.
+
+SELECT SUM(Salary),AVG(Salary),MAX(Salary),MIN(Salary) from EMPLOYEE, DEPARTMENT WHERE EMPLOYEE.Dno = DEPARTMENT.Dnumber and DEPARTMENT.Dname = 'Research';
+-- Different Approach (BETTER FOR MULTIPLE DEPARTMENTS)
+-- SELECT SUM(Salary),AVG(Salary),MAX(Salary),MIN(Salary) from EMPLOYEE, DEPARTMENT WHERE EMPLOYEE.Dno = DEPARTMENT.Dnumber GROUP BY DEPARTMENT.DNAME HAVING DEPARTMENT.Dname = 'Research';
+
+-- 3) Retrieve the number of employees in the ‘administration’ department
+SELECT COUNT(*) FROM EMPLOYEE, DEPARTMENT WHERE EMPLOYEE.Dno = DEPARTMENT.Dnumber and DEPARTMENT.Dname = 'Administration';
+
+-- 4) For each project, retrieve the project number, the project name, and the number of employees who work on that project.
+-- SELECT Pnumber, Pname, COUNT(*) from PROJECT, WORKS_ON WHERE PR0JECT.Pnumber = WORKS_ON.Pno GROUP BY Pno; Does not work for some reason
+SELECT Pnumber, Pname, COUNT(*) from PROJECT, WORKS_ON WHERE WORKS_ON.Pno=PROJECT.PNUMBER GROUP BY PNo;
+
+-- 5) For each project, retrieve the project number, the project name, project location and the number of employees from department 5 who work on the project.
+SELECT Pnumber, Pname, COUNT(*) from PROJECT, WORKS_ON WHERE WORKS_ON.Pno=PROJECT.PNUMBER AND  PROJECT.Dnum = 5 GROUP BY PNo ;
+
+-- 6) For every project located in ‘Houston’, list the project number, the controlling department number, and the department manager’s last name, address.
+-- SELECT Pnumber, Dnum, Lname, Address from PR0JECT, DEPARTMENT, EMPLOYEE WHERE PROJECT.Dnum = DEPARTMENT.Dnumber AND DEPARTMENT.Mgr_ssn = EMPLOYEE.Ssn AND PROJECT.Plocation = 'Houston'; 
+SELECT Pnumber, Dnum, Lname, Address from DEPARTMENT, PROJECT, EMPLOYEE WHERE PROJECT.Dnum = DEPARTMENT.Dnumber AND DEPARTMENT.Mgr_ssn = EMPLOYEE.Ssn AND PROJECT.Plocation = 'Houston';
+
+-- 7) Retrieve a list of employees and the projects they are working on, ordered by department and, within each department, ordered alphabetically by the first name then by last name.
+-- SELECT Fname, Lname, Pname, Dname from DEPARTMENT, PROJECT, EMPLOYEE, WORKS_ON WHERE DEPARTMENT.Dnumber = PROJECT.Dnum AND WORKS_ON.Essn = EMPLOYEE.Ssn AND WORKS_ON.Pno = PR0JECT.Pnumber ORDER BY DEPARTMENT.Dname;
+select Fname,Lname,Pname,Dname from DEPARTMENT,PROJECT,EMPLOYEE,WORKS_ON WHERE DEPARTMENT.DNUMBER=PROJECT.DNUM AND WORKS_ON.ESSN=EMPLOYEE.SSN AND WORKS_ON.PNO=PROJECT.PNUMBER ORDER BY DEPARTMENT.DNAME, FNAME, LNAME;
+
+-- 8) Retrieve the names of all employees who do not have supervisors.
+SELECT FNAME,LNAME FROM EMPLOYEE WHERE EMPLOYEE.Super_ssn IS NULL;
+
+-- 9) Retrieve the names of all employees whose supervisor’s supervisor has ‘987654321’ for Ssn.
+
+SELECT FNAME,LNAME FROM EMPLOYEE WHERE EMPLOYEE.Super_ssn IN (SELECT Ssn FROM EMPLOYEE WHERE EMPLOYEE.Super_ssn = "987654321");
+
+-- 10) Retrieve  the department name, manager name, and manager salary for every department.
+
+SELECT DName,Fname,Lname,Salary FROM DEPARTMENT,EMPLOYEE WHERE DEPARTMENT.Mgr_ssn = EMPLOYEE.Ssn;  
+
+-- 11)  Retrieve the employee name, supervisor name, and employee salary for each employee who works in the ‘Research’ department.
+
+SELECT E.Fname,E.Lname,E.Salary,S.Fname,S.Lname FROM EMPLOYEE AS E, EMPLOYEE AS S WHERE E.Super_ssn = S.ssn; 
