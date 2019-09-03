f188c2096a94418b465d0ed98e6569a46f40011c
madhumithanara <madhumithanara.171it125@nitk.edu.in>
2019-09-03 13:54:54 +0530

    13th fix

--- 171IT125/DBMS/Lab3/queries.sql
+++ 171IT125/DBMS/Lab3/queries.sql
@@ -49,7 +49,7 @@ SELECT p.Pname, d.Dname, COUNT(*), sum(w.Hours) FROM Project p, Department d, Em
 
 -- 13) Retrieve the project name, controlling department name, number of employees, and total hours worked per week on the project for each project with more than one employee working on it.
 
-SELECT p.Pnumber,p.Pname,COUNT() as employees FROM Project p, Employee e, Works_on w WHERE e.Ssn = w.Essn AND p.Pnumber = w.Pno and p.Dnum = 5 GROUP BY p.Pnumber HAVING COUNT() > 2;
+SELECT p.Pnumber,p.Pname,COUNT(*) as employees FROM Project p, Employee e, Works_on w WHERE e.Ssn = w.Essn AND p.Pnumber = w.Pno and p.Dnum = 5 GROUP BY p.Pnumber HAVING COUNT(*) > 2;
 
 -- 14) Find the names of employees who work on all the projects controlled by department number 5.
 
