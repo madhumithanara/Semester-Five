d8dd903b686c832b8c457190ef47611090880155
Madhumitha Nara <madhumithanara.171it125@nitk.edu.in>
2019-09-06 16:06:08 +0530

    DCS BSS Algorithm

---
+++ 171IT125/DCS/Lab1/server.py
@@ -0,0 +1,54 @@
+import socket 
+import threading
+import sys
+import bss
+import time
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+sock.bind(("127.0.0.1",5008))
+flag = [1,1,1]
+IP = [5008,5009,5010]
+d = {5009 : "giri" , 5010 : "hari"}
+
+def receiver(threadname,x):
+	global sock
+	while True:
+		data , add = sock.recvfrom(1024)
+		if not bss.check(data, add, flag):
+			break
+	sock.close()	
+
+bss.initialise(3,0)
+
+UDP_IP = "127.0.0.1"
+thread = threading.Thread(target = receiver , args = ("Receive" ,0 ))
+thread.start()
+print("Enter data to be sent. Enter \"exit\" to exit")
+message=input()
+while message!="exit":
+	bss.update()
+	message = message.replace(" " ,"~")
+	message += " "
+	for i in bss.timestamp:
+		message += str(i) + ","
+	message += " " + str(bss.my_index)
+	message=message.encode('utf-8')
+
+	for i in range(len(flag)):
+		if i != bss.my_index and flag[i] == 1:
+			sock.sendto(message,(UDP_IP,IP[i]))
+	message = input()
+message += " "
+bss.update()
+for i in bss.timestamp:
+	message += str(i) + ","
+message += " " + str(bss.my_index)
+
+message=message.encode('utf-8')
+for i in range(len(flag)):
+	if i!=bss.my_index and flag[i]:
+		sock.sendto(message,(UDP_IP,IP[i]))	
+sock.close()
+sys.exit(0)
+print("system exit")
+
