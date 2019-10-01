aac5b742ba31d299ff901bf45fddbb2a0beabbc7
madhumithanara <madhumithanara.171it125@nitk.edu.in>
2019-10-01 14:41:21 +0530

    Exists vs. In

--- 171IT125/DBS/Lab4/queries.sql
+++ 171IT125/DBS/Lab4/queries.sql
@@ -1,10 +1,10 @@
 SELECT pt.name AS "Patient",
        p.name AS "Primary care Physician"
-FROM patient pt
-JOIN physiciane p ON pt.pcp=p.employeeid
+FROM Patient pt
+JOIN PhysicianE p ON pt.pcp=p.employeeid
 WHERE pt.pcp NOT IN
     (SELECT head
-     FROM department);
+     FROM Department);
 
 
 
@@ -12,13 +12,13 @@ WHERE pt.pcp NOT IN
 SELECT pt.name AS "Patient",
        p.name AS "Primary Physician",
        n.name AS "Nurse"
-FROM appointment a
-JOIN patient pt ON a.patient=pt.ssn
-JOIN nurse n ON a.prepnurse=n.employeeid
-JOIN physiciane p ON pt.pcp=p.employeeid
+FROM Appointment a
+JOIN Patient pt ON a.patient=pt.ssn
+JOIN Nurse n ON a.prepnurse=n.employeeid
+JOIN PhysicianE p ON pt.pcp=p.employeeid
 WHERE a.patient IN
     (SELECT patient
-     FROM appointment a
+     FROM Appointment a
      GROUP BY a.patient
      HAVING count(*)>=2)
   AND n.registered='true'
@@ -31,10 +31,10 @@ ORDER BY pt.name;
 SELECT pt.name AS "Patient",
        p.name AS "Primary Physician",
        pd.cost AS "Procedure Cost"
-FROM patient pt
-JOIN undergoes u ON u.patient=pt.ssn
-JOIN physiciane p ON pt.pcp=p.employeeid
-JOIN procedures pd ON u.Procedures=pd.code
+FROM Patient pt
+JOIN Undergoes u ON u.patient=pt.ssn
+JOIN PhysicianE p ON pt.pcp=p.employeeid
+JOIN Procedures pd ON u.Procedures=pd.code
 WHERE pd.cost>5000;
 
 
@@ -48,17 +48,17 @@ SELECT p.name AS "Physician",
        u.DateUndergoes AS "Date of Procedure",
        pt.name AS "Patient",
        t.certificationexpires AS "Expiry Date of Certificate"
-FROM physiciane p,
-     undergoes u,
-     patient pt,
-     procedures pr,
-     trained_in t
+FROM PhysicianE p,
+     Undergoes u,
+     Patient pt,
+     Procedures pr,
+     Trained_In t
 WHERE u.patient = pt.ssn
   AND u.procedures = pr.code
   AND u.physician = p.employeeid
-  AND Pr.code = t.treatment
-  AND P.employeeid = t.physician
-  AND u.DateUndergoes > t.certificationexpires;\
+  AND pr.Code = t.treatment
+  AND p.EmployeeId = t.physician
+  AND u.DateUndergoes > t.certificationexpires;
 
 
 
@@ -68,15 +68,15 @@ SELECT p.name AS "Physician",
        pr.name AS "Procedure",
        u.dateUndergoes,
        pt.name AS "Patient"
-FROM physiciane p,
-     undergoes u,
-     patient pt,
-     procedures pr
+FROM PhysicianE p,
+     Undergoes u,
+     Patient pt,
+     Procedures pr
 WHERE u.patient = pt.SSN
   AND u.procedures = pr.Code
   AND u.physician = p.EmployeeID
   AND NOT EXISTS
     ( SELECT *
-     FROM trained_in t
+     FROM Trained_In t
      WHERE t.treatment = u.procedures
        AND t.physician = u.physician );
